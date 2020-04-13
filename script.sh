f09c16863297e7011d5dae8b756c8568a6e78ad8
holl59 <holl59@free.fr>
2020-04-13 16:47:10 +0200

    Update script.sh
    dl de zoom + install

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
-apt get update; apt install libreoffice firefox vlc zoom skypeforlinux
+sudo apt update; sudo apt install libreoffice firefox vlc
+
+* Zoom
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+
+* skypeforlinux

c3acbd95a1615213dbed2258583877e3342987d1
holl59 <holl59@free.fr>
2020-05-01 15:17:09 +0200

    Update script_lmint.sh

--- script_lmint.sh
+++ script_lmint.sh
@@ -5,8 +5,9 @@ wget https://zoom.us/client/latest/zoom_amd64.deb; sudo apt install ./zoom_amd64
 wget https://go.skype.com/skypeforlinux-64.deb; sudo apt install ./skypeforlinux-64.deb;
 
 #Anydesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - ;
-sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list;
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add - ;
+echo 'deb http://deb.anydesk.com/ all main' | sudo tee /etc/apt/sources.list.d/anydesk-stable.list;
 
 #Install
-sudo apt update; sudo apt install anydesk libreoffice libreoffice-l10n-fr brasero firefox vlc;
+sudo apt update; sudo apt upgrade;
+sudo apt install anydesk libreoffice libreoffice-l10n-fr brasero firefox vlc;
